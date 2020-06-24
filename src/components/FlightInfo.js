2e6cee6ba937a52f1b1d64920f2c195d9fbb52c2
Zachary Rizer <zacharyrizer@gmail.com>
2020-06-24 15:21:00 -0500

    media query for flights component

--- src/components/FlightInfo.js
+++ src/components/FlightInfo.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Context } from '../Context';
-import { Box, Button, Heading } from 'grommet';
+import { Box, Button, Heading, ResponsiveContext, Text } from 'grommet';
 import moment from 'moment';
 
 const FlightInfo = ({ flight }) => {
@@ -15,6 +15,7 @@ const FlightInfo = ({ flight }) => {
     price,
     flightClass,
   } = useContext(Context);
+  const size = useContext(ResponsiveContext);
   const history = useHistory();
 
   const bookFlight = () => {
@@ -34,77 +35,147 @@ const FlightInfo = ({ flight }) => {
     history.push('/checkout');
   };
   return (
-    <Box direction="column" margin={{ vertical: '1rem' }}>
-      <Box
-        direction="row"
-        justify="between"
-        align="center"
-        margin="xsmall"
-        border={{ size: 'xsmall', side: 'bottom' }}>
-        <Heading margin="small" level={4}>
-          Cruising Speed:
-        </Heading>
-        <Heading margin="small" level={4}>
-          {flight.speed}
-        </Heading>
-      </Box>
-      <Box
-        direction="row"
-        justify="between"
-        align="center"
-        margin="xsmall"
-        border={{ size: 'xsmall', side: 'bottom' }}>
-        <Heading margin="small" level={4}>
-          Total Travel Time:
-        </Heading>
-        <Heading margin="small" level={4}>
-          {flight.travelTime}
-        </Heading>
-      </Box>
-      <Box
-        direction="row"
-        justify="between"
-        align="center"
-        margin="xsmall"
-        border={{ size: 'xsmall', side: 'bottom' }}>
-        <Heading margin="small" level={4}>
-          Voyage Distance:
-        </Heading>
-        <Heading margin="small" level={4}>
-          {flight.distance}
-        </Heading>
-      </Box>
-      <Box
-        direction="row"
-        justify="between"
-        align="center"
-        margin="xsmall"
-        border={{ size: 'xsmall', side: 'bottom' }}>
-        <Heading margin="small" level={4}>
-          Price:
-        </Heading>
-        <Heading margin="small" level={4}>
-          {flight.price}
-        </Heading>
-      </Box>
-      <Box
-        direction="row"
-        justify="between"
-        align="center"
-        margin={{ vertical: 'medium' }}>
-        <Heading margin="small" level={5} color="brand">
-          Departing on {moment(date.split('T')[0]).format('MMMM Do YYYY')}
-        </Heading>
-        <Box elevation="medium">
-          <Button
-            primary
-            color={'status-warning'}
-            label="Book Voyage"
-            onClick={bookFlight}
-          />
+    <>
+      {size !== 'small' ? (
+        <Box direction="column" margin={{ vertical: '1rem' }}>
+          <Box
+            direction="row"
+            justify="between"
+            align="center"
+            margin="xsmall"
+            border={{ size: 'xsmall', side: 'bottom' }}>
+            <Heading margin="small" level={4}>
+              Cruising Speed:
+            </Heading>
+            <Heading margin="small" level={4}>
+              {flight.speed}
+            </Heading>
+          </Box>
+          <Box
+            direction="row"
+            justify="between"
+            align="center"
+            margin="xsmall"
+            border={{ size: 'xsmall', side: 'bottom' }}>
+            <Heading margin="small" level={4}>
+              Total Travel Time:
+            </Heading>
+            <Heading margin="small" level={4}>
+              {flight.travelTime}
+            </Heading>
+          </Box>
+          <Box
+            direction="row"
+            justify="between"
+            align="center"
+            margin="xsmall"
+            border={{ size: 'xsmall', side: 'bottom' }}>
+            <Heading margin="small" level={4}>
+              Voyage Distance:
+            </Heading>
+            <Heading margin="small" level={4}>
+              {flight.distance}
+            </Heading>
+          </Box>
+          <Box
+            direction="row"
+            justify="between"
+            align="center"
+            margin="xsmall"
+            border={{ size: 'xsmall', side: 'bottom' }}>
+            <Heading margin="small" level={4}>
+              Price:
+            </Heading>
+            <Heading margin="small" level={4}>
+              {flight.price}
+            </Heading>
+          </Box>
+          <Box
+            direction="row"
+            justify="between"
+            align="center"
+            margin={{ vertical: 'medium' }}>
+            <Heading margin="small" level={5} color="brand">
+              Departing on {moment(date.split('T')[0]).format('MMMM Do YYYY')}
+            </Heading>
+            <Box elevation="medium">
+              <Button
+                primary
+                color={'status-warning'}
+                label="Book Voyage"
+                onClick={bookFlight}
+              />
+            </Box>
+          </Box>
         </Box>
-      </Box>
-    </Box>
+      ) : (
+        <Box direction="column" margin="small">
+          <Box
+            direction="row"
+            justify="between"
+            align="center"
+            margin="xsmall"
+            border={{ size: 'xsmall', side: 'bottom' }}>
+            <Text margin="small" level={4}>
+              Cruising Speed:
+            </Text>
+            <Text margin="small" level={4}>
+              {flight.speed}
+            </Text>
+          </Box>
+          <Box
+            direction="row"
+            justify="between"
+            align="center"
+            margin="xsmall"
+            border={{ size: 'xsmall', side: 'bottom' }}>
+            <Text margin="small" level={4}>
+              Total Travel Time:
+            </Text>
+            <Text margin="small" level={4}>
+              {flight.travelTime}
+            </Text>
+          </Box>
+          <Box
+            direction="row"
+            justify="between"
+            align="center"
+            margin="xsmall"
+            border={{ size: 'xsmall', side: 'bottom' }}>
+            <Text margin="small" level={4}>
+              Voyage Distance:
+            </Text>
+            <Text margin="small" level={4}>
+              {flight.distance}
+            </Text>
+          </Box>
+          <Box
+            direction="row"
+            justify="between"
+            align="center"
+            margin="xsmall"
+            border={{ size: 'xsmall', side: 'bottom' }}>
+            <Text margin="small" level={4}>
+              Price:
+            </Text>
+            <Text margin="small" level={4}>
+              {flight.price}
+            </Text>
+          </Box>
+          <Text margin="small" textAlign="center" color="brand">
+            Departing on {moment(date.split('T')[0]).format('MMMM Do YYYY')}
+          </Text>
+          <Box elevation="medium" margin={{ vertical: 'small' }}>
+            <Button
+              primary
+              color={'status-warning'}
+              label="Book Voyage"
+              onClick={bookFlight}
+            />
+          </Box>
+        </Box>
+      )}
+    </>
   );
 };
 
--- src/components/Flights.js
+++ src/components/Flights.js
@@ -1,13 +1,23 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Context } from '../Context';
-import { Box, Button, Heading, Tabs, Tab } from 'grommet';
+import {
+  Box,
+  Button,
+  Heading,
+  ResponsiveContext,
+  Tabs,
+  Tab,
+  Text,
+} from 'grommet';
 import { FormNextLink } from 'grommet-icons';
+import moment from 'moment';
 
 import FlightInfo from './FlightInfo';
 
 const Flights = () => {
   const {
+    date,
     depart,
     arrive,
     distance,
@@ -16,6 +26,7 @@ const Flights = () => {
     setFlightClass,
     price,
   } = useContext(Context);
+  const size = useContext(ResponsiveContext);
   const [tab, setTab] = useState(0);
 
   const setClass = (tab) => {
@@ -36,77 +47,147 @@ const Flights = () => {
 
   return (
     <>
-      <Box
-        direction="row"
-        justify="between"
-        align="center"
-        margin={{ horizontal: '15rem' }}
-        border={{ size: 'xsmall', side: 'bottom' }}>
-        <Box direction="row" align="center">
-          <Heading margin="xsmall" level={2} color={'text-strong'}>
-            Depart:
-          </Heading>
-          <Heading margin="xsmall" level={2} color={'text-strong'}>
-            {depart}
-          </Heading>
-          <FormNextLink size="medium" />
-          <Heading margin="xsmall" level={2} color={'text-strong'}>
-            {arrive}
-          </Heading>
-        </Box>
-        <Box
-          elevation="medium"
-          margin={{
-            horizontal: 'medium',
-          }}>
-          <Link to="/">
-            <Button primary label="Modify Voyage" />
+      {size !== 'small' ? (
+        <>
+          <Box
+            direction="row"
+            justify="between"
+            align="center"
+            margin={{ horizontal: '15rem' }}
+            border={{ size: 'xsmall', side: 'bottom' }}>
+            <Box direction="row" align="center">
+              <Heading margin="xsmall" level={2} color={'text-strong'}>
+                Depart:
+              </Heading>
+              <Heading margin="xsmall" level={2} color={'text-strong'}>
+                {depart}
+              </Heading>
+              <FormNextLink size="medium" />
+              <Heading margin="xsmall" level={2} color={'text-strong'}>
+                {arrive}
+              </Heading>
+            </Box>
+            <Box
+              elevation="medium"
+              margin={{
+                horizontal: 'medium',
+              }}>
+              <Link to="/">
+                <Button primary label="Modify Voyage" />
+              </Link>
+            </Box>
+          </Box>
+          <Tabs
+            alignControls="center"
+            margin={{ horizontal: '17rem', vertical: '1rem' }}
+            onActive={(tab) => setClass(tab)}
+            activeIndex={tab}>
+            <Tab title="Economy Class" focusIndicator={false}>
+              <FlightInfo
+                flight={{
+                  speed: '25,000 km/h',
+                  travelTime: `${time.Economy} Days`,
+                  distance: `${distance} Mil. km`,
+                  price: `$${price.Economy}`,
+                }}
+              />
+            </Tab>
+            <Tab title="Premium Class" focusIndicator={false}>
+              <FlightInfo
+                flight={{
+                  speed: '50,000 km/h',
+                  travelTime: `${time.Premium} Days`,
+                  distance: `${distance} Mil. km`,
+                  price: `$${price.Premium}`,
+                }}
+              />
+            </Tab>
+          </Tabs>
+          <Link
+            to="/low-fares"
+            style={{ textDecoration: 'none' }}
+            className="button_hover">
+            <Box
+              className="button_hover"
+              direction="row"
+              justify="center"
+              align="center"
+              margin={{ horizontal: '15rem' }}
+              background="accent"
+              round="xsmall">
+              <Heading textAlign="center" level={3} className="button_hover">
+                Are Your Dates Flexible? Browse Our Lowest Fares!
+              </Heading>
+            </Box>
           </Link>
-        </Box>
-      </Box>
-      <Tabs
-        alignControls="center"
-        margin={{ horizontal: '17rem', vertical: '1rem' }}
-        onActive={(tab) => setClass(tab)}
-        activeIndex={tab}>
-        <Tab title="Economy Class" focusIndicator={false}>
-          <FlightInfo
-            flight={{
-              speed: '25,000 km/h',
-              travelTime: `${time.Economy} Days`,
-              distance: `${distance} Mil. km`,
-              price: `$${price.Economy}`,
-            }}
-          />
-        </Tab>
-        <Tab title="Premium Class" focusIndicator={false}>
-          <FlightInfo
-            flight={{
-              speed: '50,000 km/h',
-              travelTime: `${time.Premium} Days`,
-              distance: `${distance} Mil. km`,
-              price: `$${price.Premium}`,
-            }}
-          />
-        </Tab>
-      </Tabs>
-      <Link
-        to="/low-fares"
-        style={{ textDecoration: 'none' }}
-        className="button_hover">
-        <Box
-          className="button_hover"
-          direction="row"
-          justify="center"
-          align="center"
-          margin={{ horizontal: '15rem' }}
-          background="accent"
-          round="xsmall">
-          <Heading textAlign="center" level={3} className="button_hover">
-            Are Your Dates Flexible? Browse Our Lowest Fares!
-          </Heading>
-        </Box>
-      </Link>
+        </>
+      ) : (
+        <>
+          <Box
+            direction="row"
+            justify="evenly"
+            align="center"
+            border={{ size: 'xsmall', side: 'bottom' }}>
+            <Box direction="row" align="center" margin="xsmall">
+              <Text weight="bold" size="large" textAlign="center">
+                {depart} > {arrive}
+              </Text>
+            </Box>
+            <Box elevation="medium" margin="xsmall">
+              <Link to="/">
+                <Button primary label="Modify" />
+              </Link>
+            </Box>
+          </Box>
+          <Tabs
+            alignControls="center"
+            margin="small"
+            onActive={(tab) => setClass(tab)}
+            activeIndex={tab}>
+            <Tab title="Economy Class" focusIndicator={false}>
+              <FlightInfo
+                flight={{
+                  speed: '25,000 km/h',
+                  travelTime: `${time.Economy} Days`,
+                  distance: `${distance} Mil. km`,
+                  price: `$${price.Economy}`,
+                }}
+              />
+            </Tab>
+            <Tab title="Premium Class" focusIndicator={false}>
+              <FlightInfo
+                flight={{
+                  speed: '50,000 km/h',
+                  travelTime: `${time.Premium} Days`,
+                  distance: `${distance} Mil. km`,
+                  price: `$${price.Premium}`,
+                }}
+              />
+            </Tab>
+          </Tabs>
+          <Link
+            to="/low-fares"
+            style={{ textDecoration: 'none' }}
+            className="button_hover">
+            <Box
+              className="button_hover"
+              direction="column"
+              justify="center"
+              elevation="medium"
+              margin="medium"
+              align="center"
+              background="accent"
+              round="xsmall">
+              <Text textAlign="center" size="medium" className="button_hover">
+                Are Your Dates Flexible?
+              </Text>
+              <Text textAlign="center" size="medium" className="button_hover">
+                Browse Our Lowest Fares!
+              </Text>
+            </Box>
+          </Link>
+        </>
+      )}
     </>
   );
 };
