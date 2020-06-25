a2f609ff452f895df54f447c4f96e29721fb0d9a
Zachary Rizer <zacharyrizer@gmail.com>
2020-06-25 10:33:24 -0500

    phone size settings for checkout page

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { Context } from '../Context';
-import { Box, Button, Heading, Select, Text } from 'grommet';
+import { Box, Button, Heading, ResponsiveContext, Select, Text } from 'grommet';
 import { FormNextLink } from 'grommet-icons';
 import moment from 'moment';
 import { useAuth0 } from '../react-auth0-spa';
@@ -26,6 +26,7 @@ const Checkout = () => {
     flightClass,
     setFlightClass,
   } = useContext(Context);
+  const size = useContext(ResponsiveContext);
 
   const flightInfo = JSON.parse(localStorage.getItem('flightInfo'));
   const { getTokenSilently, user } = useAuth0();
@@ -90,165 +91,287 @@ const Checkout = () => {
     <>
       {!flightInfo ? null : (
         <>
-          <Box
-            direction="row"
-            justify="between"
-            align="center"
-            margin={{ horizontal: '15rem' }}
-            border={{ size: 'small', side: 'bottom', color: 'accent' }}>
-            <Box direction="row" align="center">
-              <Heading margin="small" level={2} color={'text-strong'}>
-                Trip & Flight Details
-              </Heading>
-            </Box>
-            <Box
-              elevation="medium"
-              margin={{
-                horizontal: 'medium',
-              }}>
-              <Link to="/flights">
-                <Button primary label="Back to Flight Details" />
-              </Link>
-            </Box>
-          </Box>
-          <Box
-            direction="row"
-            justify="between"
-            align="center"
-            margin={{ horizontal: '15rem' }}>
-            <Box direction="column" fill="horizontal" margin="medium">
+          {size !== 'small' ? (
+            <>
               <Box
                 direction="row"
                 justify="between"
                 align="center"
-                margin="xsmall">
-                <Text margin="small" size="medium">
-                  Departure Date:
-                </Text>
-                <Text margin="small" size="medium">
-                  {moment(flightInfo.date.split('T')[0]).format('MMMM Do YYYY')}
-                </Text>
-              </Box>
-              <Box
-                direction="row"
-                justify="between"
-                align="center"
-                margin="xsmall">
-                <Text margin="small" size="medium">
-                  Voyage Endpoints:
-                </Text>
-                <Box direction="row" justify="between" align="center">
-                  <Text margin="small" size="medium">
-                    {flightInfo.depart}
-                  </Text>
-                  <FormNextLink size="medium" />
-                  <Text margin="small" size="medium">
-                    {flightInfo.arrive}
-                  </Text>
+                margin={{ horizontal: '15rem' }}
+                border={{ size: 'small', side: 'bottom', color: 'accent' }}>
+                <Box direction="row" align="center">
+                  <Heading margin="small" level={2} color={'text-strong'}>
+                    Trip & Flight Details
+                  </Heading>
+                </Box>
+                <Box
+                  elevation="medium"
+                  margin={{
+                    horizontal: 'medium',
+                  }}>
+                  <Link to="/flights">
+                    <Button primary label="Back to Flight Details" />
+                  </Link>
                 </Box>
               </Box>
               <Box
                 direction="row"
                 justify="between"
                 align="center"
-                margin="xsmall">
-                <Text margin="small" size="medium">
-                  Total Travel Time:
-                </Text>
-                <Text margin="small" size="medium">
-                  {flightInfo.time[flightInfo.flightClass]} Days
-                </Text>
+                margin={{ horizontal: '15rem' }}>
+                <Box direction="column" fill="horizontal" margin="medium">
+                  <Box
+                    direction="row"
+                    justify="between"
+                    align="center"
+                    margin="xsmall">
+                    <Text margin="small" size="medium">
+                      Departure Date:
+                    </Text>
+                    <Text margin="small" size="medium">
+                      {moment(flightInfo.date.split('T')[0]).format(
+                        'MMMM Do YYYY'
+                      )}
+                    </Text>
+                  </Box>
+                  <Box
+                    direction="row"
+                    justify="between"
+                    align="center"
+                    margin="xsmall">
+                    <Text margin="small" size="medium">
+                      Voyage Endpoints:
+                    </Text>
+                    <Box direction="row" justify="between" align="center">
+                      <Text margin="small" size="medium">
+                        {flightInfo.depart}
+                      </Text>
+                      <FormNextLink size="medium" />
+                      <Text margin="small" size="medium">
+                        {flightInfo.arrive}
+                      </Text>
+                    </Box>
+                  </Box>
+                  <Box
+                    direction="row"
+                    justify="between"
+                    align="center"
+                    margin="xsmall">
+                    <Text margin="small" size="medium">
+                      Total Travel Time:
+                    </Text>
+                    <Text margin="small" size="medium">
+                      {flightInfo.time[flightInfo.flightClass]} Days
+                    </Text>
+                  </Box>
+                  <Box
+                    direction="row"
+                    justify="between"
+                    align="center"
+                    margin="xsmall">
+                    <Text margin="small" size="medium">
+                      Voyage Distance:
+                    </Text>
+                    <Text margin="small" size="medium">
+                      {flightInfo.distance} Mil. km
+                    </Text>
+                  </Box>
+                </Box>
+                <Box
+                  width="30rem"
+                  justify="center"
+                  fill="vertical"
+                  background="background-contrast">
+                  <Box
+                    direction="row"
+                    justify="between"
+                    alignContent="center"
+                    margin="small"
+                    border={{ size: 'small', side: 'bottom', color: 'text' }}>
+                    <Text margin="small" size="medium" color={'text-strong'}>
+                      {flightInfo.flightClass} Class
+                    </Text>
+                  </Box>
+                  <Box
+                    direction="row"
+                    justify="between"
+                    alignContent="center"
+                    margin="small"
+                    border={{ size: 'xsmall', side: 'bottom' }}>
+                    <Text margin="small" size="small">
+                      Price per Passenger:
+                    </Text>
+                    <Text margin="small" size="medium" color={'text-strong'}>
+                      ${flightInfo.price[flightInfo.flightClass]}
+                    </Text>
+                  </Box>
+                  <Box
+                    direction="row"
+                    justify="between"
+                    align="center"
+                    margin="small"
+                    border={{ size: 'xsmall', side: 'bottom' }}>
+                    <Text margin="small" size="small">
+                      Passenger(s):
+                    </Text>
+                    <Select
+                      margin={{ bottom: 'small' }}
+                      options={[1, 2, 3, 4]}
+                      style={{ width: '35px' }}
+                      value={numPass}
+                      onChange={({ option }) => setNumPass(option)}
+                    />
+                  </Box>
+                  <Box
+                    direction="row"
+                    justify="between"
+                    align="center"
+                    margin="small">
+                    <Text margin="small" size="small">
+                      Voyage Total:
+                    </Text>
+                    <Text margin="small" size="medium" color={'text-strong'}>
+                      ${flightInfo.price[flightInfo.flightClass] * numPass}
+                    </Text>
+                  </Box>
+                </Box>
               </Box>
               <Box
                 direction="row"
-                justify="between"
+                justify="center"
                 align="center"
-                margin="xsmall">
-                <Text margin="small" size="medium">
-                  Voyage Distance:
-                </Text>
-                <Text margin="small" size="medium">
-                  {flightInfo.distance} Mil. km
-                </Text>
+                elevation="large"
+                margin={{ horizontal: '15rem' }}
+                background="accent">
+                <Box margin="medium">
+                  <Button
+                    primary
+                    color={'status-warning'}
+                    label="Confirm Booking"
+                    onClick={bookFlight}
+                  />
+                </Box>
               </Box>
-            </Box>
-            <Box
-              width="30rem"
-              justify="center"
-              fill="vertical"
-              background="background-contrast">
+            </>
+          ) : (
+            <>
               <Box
                 direction="row"
-                justify="between"
-                alignContent="center"
-                margin="small"
-                border={{ size: 'small', side: 'bottom', color: 'text' }}>
-                <Text margin="small" size="medium" color={'text-strong'}>
-                  {flightInfo.flightClass} Class
-                </Text>
-              </Box>
-              <Box
-                direction="row"
-                justify="between"
-                alignContent="center"
-                margin="small"
-                border={{ size: 'xsmall', side: 'bottom' }}>
-                <Text margin="small" size="small">
-                  Price per Passenger:
-                </Text>
-                <Text margin="small" size="medium" color={'text-strong'}>
-                  ${flightInfo.price[flightInfo.flightClass]}
-                </Text>
-              </Box>
-              <Box
-                direction="row"
-                justify="between"
+                justify="center"
                 align="center"
-                margin="small"
-                border={{ size: 'xsmall', side: 'bottom' }}>
-                <Text margin="small" size="small">
-                  Passenger(s):
-                </Text>
-                <Select
-                  margin={{ bottom: 'small' }}
-                  options={[1, 2, 3, 4]}
-                  style={{ width: '35px' }}
-                  value={numPass}
-                  onChange={({ option }) => setNumPass(option)}
-                />
+                border={{ size: 'xsmall', side: 'bottom', color: 'accent' }}>
+                <Heading margin="small" level={6} color={'text-strong'}>
+                  Trip & Flight Details
+                </Heading>
+              </Box>
+              <Box direction="column" margin="xsmall">
+                <Box
+                  direction="row"
+                  justify="between"
+                  align="center"
+                  margin={{ vertical: 'medium' }}>
+                  <Text>Departure Date:</Text>
+                  <Text>
+                    {moment(flightInfo.date.split('T')[0]).format(
+                      'MMMM Do YYYY'
+                    )}
+                  </Text>
+                </Box>
+                <Box
+                  direction="row"
+                  justify="between"
+                  align="center"
+                  margin={{ vertical: 'medium' }}>
+                  <Text>Voyage Endpoints:</Text>
+                  <Box direction="row" justify="between" align="center">
+                    <Text>{flightInfo.depart}</Text>
+                    <FormNextLink size="small" />
+                    <Text>{flightInfo.arrive}</Text>
+                  </Box>
+                </Box>
+                <Box
+                  direction="row"
+                  justify="between"
+                  align="center"
+                  margin={{ vertical: 'medium' }}>
+                  <Text>Total Travel Time:</Text>
+                  <Text>{flightInfo.time[flightInfo.flightClass]} Days</Text>
+                </Box>
+                <Box
+                  direction="row"
+                  justify="between"
+                  align="center"
+                  margin={{ vertical: 'medium' }}>
+                  <Text>Voyage Distance:</Text>
+                  <Text>{flightInfo.distance} Mil. km</Text>
+                </Box>
+              </Box>
+              <Box justify="center" background="background-contrast">
+                <Box
+                  direction="row"
+                  justify="between"
+                  alignContent="center"
+                  margin="xsmall"
+                  border={{ size: 'small', side: 'bottom', color: 'text' }}>
+                  <Text margin="xsmall" color={'text-strong'}>
+                    {flightInfo.flightClass} Class
+                  </Text>
+                </Box>
+                <Box
+                  direction="row"
+                  justify="between"
+                  alignContent="center"
+                  margin="xsmall"
+                  border={{ size: 'xsmall', side: 'bottom' }}>
+                  <Text margin="xsmall">Price per Passenger:</Text>
+                  <Text margin="xsmall" color={'text-strong'}>
+                    ${flightInfo.price[flightInfo.flightClass]}
+                  </Text>
+                </Box>
+                <Box
+                  direction="row"
+                  justify="between"
+                  align="center"
+                  margin="xsmall"
+                  border={{ size: 'xsmall', side: 'bottom' }}>
+                  <Text margin="xsmall">Passenger(s):</Text>
+                  <Select
+                    margin={{ bottom: 'small' }}
+                    options={[1, 2, 3, 4]}
+                    style={{ width: '35px' }}
+                    value={numPass}
+                    onChange={({ option }) => setNumPass(option)}
+                  />
+                </Box>
+                <Box
+                  direction="row"
+                  justify="between"
+                  align="center"
+                  margin="xsmall">
+                  <Text margin="xsmall">Voyage Total:</Text>
+                  <Text margin="xsmall" color={'text-strong'}>
+                    ${flightInfo.price[flightInfo.flightClass] * numPass}
+                  </Text>
+                </Box>
               </Box>
               <Box
                 direction="row"
-                justify="between"
+                justify="center"
                 align="center"
-                margin="small">
-                <Text margin="small" size="small">
-                  Voyage Total:
-                </Text>
-                <Text margin="small" size="medium" color={'text-strong'}>
-                  $
-                  {flightInfo.price[flightInfo.flightClass] *
-                    flightInfo.numPass}
-                </Text>
+                elevation="medium"
+                margin="small"
+                background="accent">
+                <Box margin="small">
+                  <Button
+                    primary
+                    color={'status-warning'}
+                    label="Confirm Booking"
+                    onClick={bookFlight}
+                  />
+                </Box>
               </Box>
-            </Box>
-          </Box>
-          <Box
-            direction="row"
-            justify="center"
-            align="center"
-            elevation="large"
-            margin={{ horizontal: '15rem' }}
-            background="accent">
-            <Box margin="medium">
-              <Button
-                primary
-                color={'status-warning'}
-                label="Confirm Booking"
-                onClick={bookFlight}
-              />
-            </Box>
-          </Box>
+            </>
+          )}
         </>
       )}
     </>
